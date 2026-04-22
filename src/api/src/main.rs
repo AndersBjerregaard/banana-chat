0984b963f56373e0da66841c3fe1de7ff0a19f10
Anders Nielsen <don-sbn@hotmail.com>
2026-04-22 13:11:17 +0200

    server init message

--- src/api/src/main.rs
+++ src/api/src/main.rs
@@ -1,4 +1,4 @@
-use  axum::{
+use axum::{
     routing::get,
     Router,
 };
@@ -7,5 +7,6 @@ use  axum::{
 async fn main() {
     let app = Router::new().route("/", get(|| async { "Hello, World!" } ));
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    println!("Server up! Listening on: 0.0.0.0:3000");
     axum::serve(listener, app).await.unwrap();
 }
