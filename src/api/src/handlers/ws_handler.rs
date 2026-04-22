9ac2ed8dc64e51a2cf84c2c4a11991330bbe4b6f
Anders Nielsen <don-sbn@hotmail.com>
2026-04-22 19:14:12 +0200

    username and timestamp for messages

--- src/api/src/handlers/ws_handler.rs
+++ src/api/src/handlers/ws_handler.rs
@@ -2,7 +2,9 @@ use axum::{
     extract::{Path, State, ws::{Message, WebSocket, WebSocketUpgrade}},
     response::IntoResponse,
 };
-use futures_util::{StreamExt, SinkExt};
+use chrono::{DateTime, Local};
+use futures_util::{SinkExt, StreamExt, stream::{SplitSink, SplitStream}};
+use tokio::sync::broadcast::Receiver;
 use crate::state::SharedState;
 
 pub async fn ws_handler(
@@ -18,12 +20,15 @@ async fn handle_socket(
     state: SharedState,
     username: String
 ) {
-    let (mut sender, _) = socket.split();
-    let mut rx = state.hub.tx.subscribe();
-    println!("User {} established connection.", username);
+    let (mut sender, _): (SplitSink<WebSocket, Message>, SplitStream<WebSocket>) = socket.split();
+    let mut rx: Receiver<String> = state.hub.tx.subscribe();
+    let con_local_now: DateTime<Local> = Local::now();
+    println!("[{}] User {} established connection.", con_local_now, username);
 
     while let Ok(msg) = rx.recv().await {
-        if sender.send(Message::Text(msg.into())).await.is_err() {
+        let local_now: DateTime<Local> = Local::now();
+        let full_message: String = format!("[{}] {}: {}", local_now, username, msg);
+        if sender.send(Message::Text(full_message.into())).await.is_err() {
             break;
         }
     }
