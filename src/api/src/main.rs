6c224fb7f78c4929c0bbbf31e5d0e581025fbd43
Anders Stræde Bjerregaard Nielsen <don-sbn@hotmail.com>
2026-05-09 18:53:32 +0200

    added cors layer to api

--- src/api/src/main.rs
+++ src/api/src/main.rs
@@ -1,6 +1,7 @@
 use std::sync::Arc;
 use banana_chat::{routes, state::{AppHub, AppState}};
 use tokio::sync::broadcast;
+use tower_http::cors::{Any, CorsLayer};
 
 #[tokio::main]
 async fn main() {
@@ -8,8 +9,13 @@ async fn main() {
     let state = Arc::new(AppState {
         hub: AppHub { tx },
     });
+    
+    let cors: CorsLayer = CorsLayer::new()
+        .allow_origin(Any)
+        .allow_methods(Any)
+        .allow_headers(Any);
 
-    let app = routes::create_router(state);
+    let app = routes::create_router(state, cors);
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
     println!("🚀 Server started on 0.0.0.0:3000");
--- src/api/src/routes/mod.rs
+++ src/api/src/routes/mod.rs
@@ -1,12 +1,14 @@
 use axum::Router;
+use tower_http::cors::CorsLayer;
 use crate::state::SharedState;
 
 mod websocket;
 mod notify;
 
-pub fn create_router(state: SharedState) -> Router {
+pub fn create_router(state: SharedState, cors: CorsLayer) -> Router {
     Router::new()
         .nest("/ws", websocket::routes())
         .nest("/notify", notify::routes())
+        .layer(cors)
         .with_state(state)
 }
