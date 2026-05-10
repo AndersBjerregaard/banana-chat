c17c0758597fb86d63622953e2976a4206dcbe5e
Anders Nielsen <don-sbn@hotmail.com>
2026-05-10 12:42:22 +0200

    proper cli benchmark test

--- tests/local_benchmark/src/main.rs
+++ tests/local_benchmark/src/main.rs
@@ -1,7 +1,9 @@
-use std::{env, thread::sleep, time::{Duration, Instant}, u64};
-
+use std::{env, time::{Duration, Instant}};
 use hdrhistogram::Histogram;
 use reqwest::Client;
+use futures_util::{SinkExt, StreamExt, TryStreamExt};
+use tokio_tungstenite::{MaybeTlsStream, WebSocketStream, tungstenite::{http::Response}};
+use tokio::{net::TcpStream};
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -24,7 +26,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     println!("Pinging server...");
 
-    let client = Client::new();
+let client = Client::new();
 
     match client
         .head(ws_url)
@@ -41,15 +43,37 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     let full = Instant::now();
 
-    let mut hist: Histogram<u64> = Histogram::<u64>::new_with_bounds(1, 60 * 60 * 100, 2).unwrap();
+    // Histogram over microsecond precision
+    let mut hist: Histogram<u64> = Histogram::<u64>::new_with_bounds(1, 60 * 60 * 1_000_000, 3).unwrap();
 
     for _ in 0..iterations {
         let start = Instant::now();
 
-        // Do some magic
-        let dur: Duration = Duration::from_millis(16);
+        let ws_full_url: &str = "ws://localhost:3000/ws/subscribe/testuser";
 
-        sleep(dur);
+        let (ws_stream, _): (WebSocketStream<MaybeTlsStream<TcpStream>>, Response<Option<Vec<u8>>>) = tokio_tungstenite::connect_async(ws_full_url).await?;
+        let (mut write, mut read) = ws_stream.split();
+
+        let notify_url: &str = "http://localhost:3000/notify/hello";
+
+        match client
+            .post(notify_url)
+            .header("x-user", "test")
+            .send()
+            .await {
+                Ok(resp) if !resp.status().is_success() => {
+                    panic!("Server did not respond with success status code: {}", resp.status());
+                }
+                Err(e) => panic!("Error trying to reach server: {0}", e),
+                _ => {} // Success
+            };
+
+        match read.try_next().await {
+            Ok(_) => {} // Success,
+            Err(e) => panic!("Error from server: {}", e),
+        }
+
+        write.close().await?;
 
         hist.record(start.elapsed().as_micros() as u64)?;
     }
@@ -57,10 +81,10 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let elapsed: Duration = full.elapsed();
 
     println!("Benchmark took {0} ms", elapsed.as_millis());
-    println!("p50: {0}", hist.value_at_quantile(0.5));
-    println!("p95: {0}", hist.value_at_quantile(0.95));
-    println!("p99: {0}", hist.value_at_quantile(0.99));
-    println!("max: {0}", hist.max());
+    println!("p50: {0} µs", hist.value_at_quantile(0.5));
+    println!("p95: {0} µs", hist.value_at_quantile(0.95));
+    println!("p99: {0} µs", hist.value_at_quantile(0.99));
+    println!("max: {0} µs", hist.max());
 
     Ok(())
 }
