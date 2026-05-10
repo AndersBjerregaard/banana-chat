452faf1c4ee3e0aee5bfbcf82d25bfd06eb72ce5
Anders Nielsen <don-sbn@hotmail.com>
2026-05-10 10:58:59 +0200

    HEAD route for websocket endpoint

--- src/api/src/routes/websocket.rs
+++ src/api/src/routes/websocket.rs
@@ -1,7 +1,11 @@
+use axum::http::StatusCode;
+use axum::routing::head;
 use axum::{routing::get, Router};
 use crate::handlers::ws_handler::ws_handler;
 use crate::state::SharedState;
 
 pub fn routes() -> Router<SharedState> {
-    Router::new().route("/subscribe/{username}", get(ws_handler))
+    Router::new()
+        .route("/subscribe/{username}", get(ws_handler))
+        .route("/subscribe", head(|| async { StatusCode::OK }))
 }
